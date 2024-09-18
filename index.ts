ac6bb69ca64a525411e45747f61b26feccef551f
Nicolas Seynaeve <nicolas.sey@gmail.com>
2024-09-18 17:42:35 +0200

    add index

---
+++ index.ts
@@ -0,0 +1,5 @@
+import frenchRegions from './data/regions.json';
+import nations from './data/nations.json';
+import frenchDepartments from './data/departements.json';
+
+export { frenchDepartments, frenchRegions, nations };
